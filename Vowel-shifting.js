0df703c284909e30043d305069c15e0133da046d
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-08-01 19:28:49 -0700

    Update Vowel-shifting.js

--- Vowel-shifting.js
+++ Vowel-shifting.js
@@ -33,3 +33,19 @@ const vowelShift = (text, n) => {
 };
 
 // or
+
+const vowelShifter = (text, n) => {
+  if(text === null) return null
+  const v = 'aeiouAEIOU'
+  const vowels = [...text].filter(c => v.includes(c))
+  
+  if(n > 0){
+    while(n--) vowels.unshift(vowels.pop())
+  }else{
+    while(n++) vowels.push(vowels.shift())
+  }
+  
+  vowels.reverse()
+  
+  return [...text].map(c => v.includes(c) ? vowels.pop() : c).join('')
+}
